a6fea1e5b2786fa1582a8dbaa99c7d7b545b424b
Ridwandarwis <139984446+Ridwandarwis@users.noreply.github.com>
2023-10-06 14:27:45 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from main import predict_page, tab
+from explore import show_explore_page
+from Home import home
+
+
+def explore():
+    selection = st.sidebar.selectbox(
+        ":Yellow[What do you want to know about my project?]",
+        ("Home","Make predictions", "Explore Data"),
+    )
+    return selection
+
+
+# def img():
+#     image_object = st.sidebar.image(
+#         "https://cdn.analyticsvidhya.com/wp-content/uploads/2020/05/Churn-Prediction-scaled.jpg"
+#     )
+#     return image_object
+
+if __name__ == "__main__":
+    ok = explore()
+    # img()
+
+    if ok == "Make predictions":
+        predict_page()
+    elif ok == "Home":
+        home()
+    else:
+        show_explore_page()
+
+    # tab()
