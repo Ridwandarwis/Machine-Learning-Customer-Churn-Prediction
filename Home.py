05819e107f7184b3990833e331bca4ac2a5517c5
Ridwandarwis <139984446+Ridwandarwis@users.noreply.github.com>
2023-10-30 20:34:25 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,10 +2,8 @@ import streamlit as st
 
 def home():
     Ridwan_Darwis = '[Muh. Ridwan Darwis](https://www.linkedin.com/in/ridwandarwis)'
-    Rizqi_Rivah = '[Muhamad Rizqi Rivah](https://www.linkedin.com/in/rizqirivah-7b0210272/)'
 
-    st.subheader('Final Project Data Enthusiast')
-    st.image('FP.png', caption='Meet Our Teams!', use_column_width=None)
-    st.write('**Lets Connect With Our Team on Linkedin:**')
+    st.subheader('Customer Churn Analysis')
+    st.image('FP.png', caption='Muh. Ridwan Darwis', use_column_width=None)
+    st.write('**Lets Connect With me on Linkedin:**')
     st.markdown(Ridwan_Darwis, unsafe_allow_html=True)
-    st.markdown(Rizqi_Rivah, unsafe_allow_html=True)
