f72c30c0f1230a4f8892e0c5f33d9bb6f7778a89
Ridwandarwis <139984446+Ridwandarwis@users.noreply.github.com>
2023-10-31 13:59:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from main import predict_page, tab
 from Home import home
+from main import predict_page, tab
+
 
 
 def explore():
@@ -11,19 +12,12 @@ def explore():
     return selection
 
 
-# def img():
-#     image_object = st.sidebar.image(
-#         "https://cdn.analyticsvidhya.com/wp-content/uploads/2020/05/Churn-Prediction-scaled.jpg"
-#     )
-#     return image_object
-
 if __name__ == "__main__":
     ok = explore()
-    # img()
 
     if ok == "Make predictions":
         predict_page()
     else:
         home()
 
-    # tab()
+    tab()
